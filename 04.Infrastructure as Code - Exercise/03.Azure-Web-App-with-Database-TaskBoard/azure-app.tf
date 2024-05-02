98a58e2a270ecfbdd22fedc77beea2ce8724b15f
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-05-02 03:33:28 +0300

    Azure-Web-App-with-Database-TaskBoard

---
+++ 04.Infrastructure as Code - Exercise/03.Azure-Web-App-with-Database-TaskBoard/azure-app.tf
@@ -0,0 +1,93 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.90.0"
+    }
+  }
+  # in order to add tfstate to the cloud, we need to add this
+  # you have to create the recource group and storage account
+  # backend "azurerm" {
+  #   resource_group_name = "StorageRG"
+  #   storage_account_name = "taskboardstoragechibi"
+  #   container_name = "taskboardcontainer"
+  #   key = "taskboard.tfstate"
+  # }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# resource "random_integer" "ri" {
+#   min = 10000
+#   max = 99999
+# }
+
+# create resource group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
+# create Linux App Service Plan
+resource "azurerm_service_plan" "asp" {
+  name                = var.app_service_plan_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_mssql_server" "sqlserver" {
+  name                         = var.sql_server_name
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = var.sql_admin_login
+  administrator_login_password = var.sql_admin_password
+}
+
+resource "azurerm_mssql_database" "database" {
+  name           = var.sql_database_name
+  server_id      = azurerm_mssql_server.sqlserver.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 2
+  sku_name       = "S0"
+  zone_redundant = false
+}
+
+resource "azurerm_mssql_firewall_rule" "firewall" {
+  name             = var.firewall_rule_name
+  server_id        = azurerm_mssql_server.sqlserver.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+# create the web app
+resource "azurerm_linux_web_app" "app" {
+  name                = var.app_service_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.asp.id
+  connection_string {
+    name  = "DefaultConnection"
+    type  = "SQLAzure"
+    value = "Data Source=tcp:${azurerm_mssql_server.sqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.database.name};User ID=${azurerm_mssql_server.sqlserver.administrator_login};Password=${azurerm_mssql_server.sqlserver.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
+  }
+
+  site_config {
+    application_stack {
+      dotnet_version = "6.0"
+    }
+    always_on = false
+  }
+}
+
+# deploy code from a public GitHub repo
+resource "azurerm_app_service_source_control" "sourcecontrol" {
+  app_id                 = azurerm_linux_web_app.app.id
+  repo_url               = var.repo_url
+  branch                 = "main"
+  use_manual_integration = true
+}
